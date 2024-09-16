7574ae77e19b372b0f603010ac943b78377080b7
Shivani Maurya <shivani.shivi333@gmail.com>
2024-09-16 22:21:36 +0530

    Add Blog component

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -2,12 +2,17 @@
 import { useState } from "react";
 export default function Blog(){
     
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+    
+    const[formData, setFormData] = useState({title:"", content:"" });
+    const [blogs, setBlogs] = useState([]);
     function handleSubmit(e){
         e.preventDefault();
+        setBlogs([{ title: formData.title, content: formData.content}, ...blogs]);
+        setFormData({title:"", content:""})
+        
+    console.log(blogs);
     }
-
+    
     return(
         <>
         {/* Heading of the page */}
@@ -23,16 +28,16 @@ export default function Blog(){
                 <Row label="Title">
                         <input className="input"
                                 placeholder="Enter the Title of Blog..."
-                                value ={title}
-                                onChange={(e)=>setTitle(e.target.value)}/>
+                                value ={formData.title}
+                                onChange={(e)=>setFormData({title: e.target.value, content:formData.content})}/>
                 </Row >
 
                 {/* Row component to create a row for Text area field */}
                 <Row label="Content">
                         <textarea className="input content"
                                 placeholder="Content for Blogs...."
-                                value ={content}
-                                onChange={(e)=>setContent(e.target.value)}/>
+                                value ={formData.content}
+                                onChange={(e)=>setFormData({title: formData.title, content: e.target.value})}/>
                 </Row >
 
                 {/* Button to submit the blog */}            
@@ -45,8 +50,13 @@ export default function Blog(){
 
         {/* Section where submitted blogs will be displayed */}
         <h2> Blogs </h2>
-        <h3>Title of Blog --> {title}</h3>
-        <p>{content}</p>
+        {blogs.map((blog, i)=>(
+            <div className="blog" key={i}>
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+            </div>
+        ))}
+        
 
         
         </>
